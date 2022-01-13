13d129e917281cb1081960b7db6fe26deed25cbb
skipGit <komanitskiygit@gmail.com>
2022-01-13 13:42:20 +0200

    Connecting Postgre.
    Create, connect and config db.
    Generate controller, module, service files for further development.

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,26 @@
 import { Module } from "@nestjs/common";
+import { SequelizeModule } from "@nestjs/sequelize";
+import { TodosModule } from "./todos/todos.module";
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-
+  controllers: [],
+  providers: [],
+  imports: [
+    ConfigModule.forRoot({
+        envFilePath: '.env'
+    }),
+    SequelizeModule.forRoot({
+      dialect: "postgres",
+      host: process.env.POSTGRES_HOST,
+      port: Number(process.env.POSTGRES_PORT),
+      username: process.env.POSTGRES_USER,
+      password: process.env.POSTGRES_PASS,
+      database: process.env.POSTGRES_DB,
+      models: [],
+      autoLoadModels: true,
+    }),
+    TodosModule,
+  ],
 })
-
 export class AppModule {}
